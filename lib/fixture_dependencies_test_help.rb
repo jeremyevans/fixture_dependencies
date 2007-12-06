c148122b7bdd5f527464cb69623aa6d0dbcdc379
Jeremy Evans <code@jeremyevans.net>
2007-12-06 20:43:42 +0000

    Make most FixtureDependencies class methods private, and add RDoc

--- lib/fixture_dependencies_test_help.rb
+++ lib/fixture_dependencies_test_help.rb
@@ -7,6 +7,7 @@ module Test
       def self.method_added(x)
       end
       
+      # Load fixtures using FixtureDependencies inside a transaction
       def setup_with_fixtures
         ActiveRecord::Base.send :increment_open_transactions
         ActiveRecord::Base.connection.begin_db_transaction
@@ -19,10 +20,12 @@ module Test
       end
       
       private
+        # Load fixtures named with the fixtures class method
         def load_fixtures
           load(*fixture_table_names)
         end
         
+        # Load given fixtures using FixtureDependencies
         def load(*fixture)
           FixtureDependencies.load(*fixture)
         end
