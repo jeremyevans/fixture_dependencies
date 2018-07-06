41849ac37204f343a43ab4c005146cce45397b09
Jeremy Evans <code@jeremyevans.net>
2018-07-06 09:14:13 -0700

    Don't load minitest plugins when testing

--- spec/fd_spec.rb
+++ spec/fd_spec.rb
@@ -1,4 +1,7 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), 'spec_helper')
+
+ENV['MT_NO_PLUGINS'] = '1' # Work around stupid autoloading of plugins
+gem 'minitest'
 require 'minitest/autorun'
 
 describe FixtureDependencies do
