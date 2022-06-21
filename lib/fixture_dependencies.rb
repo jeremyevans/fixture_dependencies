81ca8475a7557088bf080e55bc51c79de8bed5a5
Jeremy Evans <code@jeremyevans.net>
2022-06-21 14:47:16 -0700

    Remove support for Sequel <4.24.0
    sti_load was added to the Sequel single_table_inheritance plugin
    in 3.9.0, and the class_table_inheritance started using
    single_table_inheritance in Sequel 4.24.0, so after Sequel 4.24.0,
    these branches are not reachable.  Sequel 4.24.0 is 7 years old
    at this point, so I'm comfortable dropping compatibility with it.

--- lib/fixture_dependencies.rb
+++ lib/fixture_dependencies.rb
@@ -191,16 +191,6 @@ class << FixtureDependencies
       obj = model.sti_load(model.sti_key => attributes[model.sti_key])
       obj.send(:initialize)
       model = obj.model
-    elsif model.respond_to?(:sti_key)
-      obj = attributes[model.sti_key].to_s.camelize.constantize.new
-    elsif model.respond_to?(:cti_key) # support for Sequel's pre-4.24.0 hybrid CTI support
-      mv = attributes[model.cti_key]
-      if (mm = model.cti_model_map)
-        model = mm[mv].to_s.constantize
-      elsif !mv.nil?
-        model = mv.constantize
-      end
-      obj = model.new
     else
       obj = model.new
     end
